e2a94df6dd2f74046c3f2832971b10a30212e46b
Tutlix <tomstvintikis@gmail.com>
2026-01-24 16:12:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("downloadBtn")?.addEventListener("click", function() {
+  window.location.href = "#download";
+});
+
+document.getElementById("downloadBtn2")?.addEventListener("click", function() {
+  window.location.href = "https://mr-tutlix.itch.io/fortroyal-2d";
+});
